fdc9f708886d660a67f279118fdc471d5c15ec28
Anny <annylianmarian@gmail.com>
2020-10-29 12:12:07 +0100

    tareas A

--- src/Logo.js
+++ src/Logo.js
@@ -2,11 +2,16 @@ import React from "react";
 
 const Logo = (props) => {
     console.log(props);
+    const logWhenClicked =() =>{
+        console.log("My Pokemón")
+        
+}
     return (
         <header>
             <h1> "Bienvenido a la <span>{props.appName}"</span></h1>  
-            <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png"/>
+            <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png" alt="Imagen de Pokemón" onClick={logWhenClicked}/>
         </header> 
     )   
 }
+
 export default Logo;
